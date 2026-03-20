c1b82ad9f88a2bcc2b817177534c37a95d275655
32780542 <139351788+32780542@users.noreply.github.com>
2026-03-20 15:38:22 +0800

    Delete build directory

--- build/cav_traj_gen/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/siegf/Downloads/path_planning_test/TrajPlanRace/src/cav_traj_gen
-# Build directory: /home/siegf/Downloads/path_planning_test/TrajPlanRace/build/cav_traj_gen
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/siegf/Downloads/path_planning_test/TrajPlanRace/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
